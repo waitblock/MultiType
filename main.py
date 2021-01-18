27b5c6d7e0b893830080d6f1542f2d104ca22f49
Ethan <65051086+waitblock@users.noreply.github.com>
2021-01-17 20:19:25 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -4,39 +4,51 @@ word_file = open("words.txt", "r")
 words = word_file.read().split("\n")
 word_file.close()
 
-times = []
+print("MultiType, the Multipurpose Typing Program")
+print("Press Control-C to quit.")
+print("\n")
 
-start_delay = 3
+while True:
+    try:
+
+        words_amount = input("Enter the amount of words: ")
+
+        on_start = input("Press the return key to start: ")
+
+        delays = []
+
+        start_delay = 3
 
 
 
-for i in range(start_delay):
-    print("The test will start in", (start_delay))
-    time.sleep(1)
-    start_delay -= 1
+        for i in range(start_delay):
+            print("The test will start in", str(start_delay)+"...")
+            time.sleep(1)
+            start_delay -= 1
 
-print("Test started.")
+        print("Test started.")
 
-correct_words = 0
-total_words = 0
+        correct_words = 0
+        total_words = 0
 
-start_epoch = calendar.timegm(time.gmtime())
-for i in range(30):
-    index = random.randint(0, len(words))
-    start_epoch = calendar.timegm(time.gmtime())
-    typed = input("Type "+words[index]+": ")
-    times.append(calendar.timegm(time.gmtime())-start_epoch)
-    if typed == words[index]:
-        correct_words += 1
-    total_words += 1
+        start_epoch = calendar.timegm(time.gmtime())
 
-sum = 0
-for i in range(len(times)):
-    sum += times[i]
+        for i in range(words_amount):
+            index = random.randint(0, len(words))
+            typed = input("Type "+words[index]+": ")
+            if typed == words[index]:
+                correct_words += 1
+            total_words += 1
 
-avg = sum/len(times)
+        sum_delays = 0
+        for i in range(len(delays)):
+            sum_delays += delays[i]
 
-print("Average time per word: "+str(avg)+"sec")
-print("WPM: "+str(avg*60))
-print("Total words typed correctly: "+str(correct_words))
-print("Total words typed: "+str(total_words))
+        raw_time = calendar.timegm(time.gmtime())-start_epoch
+        time = calendar.timegm(time.gmtime())-start_epoch - sum_delays
+
+        print("Raw time: "+str(raw_time)+" sec")
+        print("Adjusted time: "+str(time)+" sec")
+        print("Score: "+str(correct_words)+"/30")
+    except KeyboardInterrupt:
+        exit()

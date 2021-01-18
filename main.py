7cc9c2f1fa56f789c93acd1a8a0c2fa199377689
Ethan <65051086+waitblock@users.noreply.github.com>
2021-01-17 23:21:55 -0500

    update - added word practice and filtered out some more words

--- main.py
+++ main.py
@@ -6,49 +6,65 @@ word_file.close()
 
 print("MultiType, the Multipurpose Typing Program")
 print("Press Control-C to quit.")
-print("\n")
 
 while True:
     try:
+        print("\n")
+        print("1. Word Test")
+        print("2. Word Practice")
+        option = input("Select an option: ")
 
-        words_amount = input("Enter the amount of words: ")
+        if option == "1":
+            words_amount = int(input("Enter the amount of words: "))
 
-        on_start = input("Press the return key to start: ")
+            on_start = input("Press the return key to start: ")
 
-        delays = []
+            start_delay = 3
 
-        start_delay = 3
+            for i in range(start_delay):
+                print("The test will start in", str(start_delay)+"...")
+                time.sleep(1)
+                start_delay -= 1
 
+            print("Test started.")
 
+            correct_words = 0
+            total_words = 0
 
-        for i in range(start_delay):
-            print("The test will start in", str(start_delay)+"...")
-            time.sleep(1)
-            start_delay -= 1
+            start_epoch = calendar.timegm(time.gmtime())
 
-        print("Test started.")
+            for i in range(words_amount):
+                index = random.randint(0, len(words))
+                typed = input("Type "+words[index]+": ")
+                if typed == words[index]:
+                    correct_words += 1
+                total_words += 1
 
-        correct_words = 0
-        total_words = 0
+            total_time = calendar.timegm(time.gmtime())-start_epoch
+            
+            print("Time: "+str(total_time)+" sec")
+            print("Score: "+str(correct_words)+"/"+str(words_amount))
 
-        start_epoch = calendar.timegm(time.gmtime())
+        if option == "2":
+            print("Press Control-C to stop the practice.")
+            start_epoch = calendar.timegm(time.gmtime())
+            correct_words = 0
+            total_words = 0
+            while True:
+                try:
+                    index = random.randint(0,len(words))
+                    typed = input("Type "+words[index]+": ")
+                    if typed == words[index]:
+                        correct_words += 1
+                    total_words += 1
+                except KeyboardInterrupt:
+                    break
+            total_time = calendar.timegm(time.gmtime())-start_epoch
 
-        for i in range(words_amount):
-            index = random.randint(0, len(words))
-            typed = input("Type "+words[index]+": ")
-            if typed == words[index]:
-                correct_words += 1
-            total_words += 1
-
-        sum_delays = 0
-        for i in range(len(delays)):
-            sum_delays += delays[i]
-
-        raw_time = calendar.timegm(time.gmtime())-start_epoch
-        time = calendar.timegm(time.gmtime())-start_epoch - sum_delays
-
-        print("Raw time: "+str(raw_time)+" sec")
-        print("Adjusted time: "+str(time)+" sec")
-        print("Score: "+str(correct_words)+"/30")
+            print("Time Spent Practicing: "+str(total_time)+" sec")
+            print("Words Typed Correctly: "+str(correct_words)+" words")
+            print("Total Words Typed: "+str(total_words)+" words")
+        
     except KeyboardInterrupt:
-        exit()
+        print("Exiting...")
+        break
